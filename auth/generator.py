aa2633b26ae037d623cebb33aeb460b3cb6ca4b9
heurist-network <cyberpenguin3@protonmail.com>
2024-05-12 06:54:40 -0700

    fix: handle composite address in generator.py to allow GPU tagging

--- auth/generator.py
+++ auth/generator.py
@@ -36,8 +36,11 @@ class WalletGenerator:
         print(table)
         print("Make sure you back up the identity wallet file. One identity wallet is bound to one miner address receiving rewards. The identity wallet is used for authentication only. DO NOT deposit funds into the identity wallet.\n")
 
+    def fetch_iw_address(self, rw_address):
+        return self.contract.functions.identityAddress(Web3.to_checksum_address(rw_address)).call()
+
     def is_bind(self, rw_address):
-        return self.contract.functions.identityAddress(rw_address).call() != '0x0000000000000000000000000000000000000000'
+        return '0x0000000000000000000000000000000000000000' != self.fetch_iw_address(rw_address)
 
     def read_wallet_file(self, file_path):
         with open(file_path, 'r') as file:
@@ -54,7 +57,7 @@ class WalletGenerator:
         for key, value in os.environ.items():
             if key.startswith('MINER_ID_'):
                 miner_id = value
-                rw_address = miner_id  # Use miner_id as the reward wallet address
+                rw_address = miner_id.split('-')[0].lower()  # Extract the address
                 file_path = os.path.join(self.keys_dir, f'{miner_id.lower()}.txt')
 
                 if not self.is_bind(rw_address):
@@ -62,7 +65,7 @@ class WalletGenerator:
                     if os.path.exists(file_path):
                         # Check if the [RW].txt file exists
                         seed_phrase, iw_address = self.read_wallet_file(file_path)
-                        print(f"\033[1mMiner ID: {miner_id}\033[0m - Warning: Identity wallet file for {rw_address} is found, but binding is not established on-chain.")
+                        print(f"\033[1mMiner ID: {miner_id}\033[0m - Warning: Identity wallet file for Reward Wallet {rw_address} is found, but binding is not established on-chain.")
                     else:
                         print(f"\033[1mMiner ID: {miner_id}\033[0m - No binding identity wallet, generating one...")
                         mnemo = Mnemonic("english")
@@ -83,7 +86,7 @@ class WalletGenerator:
                         account = self.w3.eth.account.from_mnemonic(seed_phrase)
                         imported_iw_address = account.address
 
-                        bind_iw_address = self.contract.functions.identityAddress(rw_address).call()
+                        bind_iw_address = self.fetch_iw_address(rw_address)
                         while imported_iw_address.lower() != bind_iw_address.lower():  # Compare in lowercase
                             print("Imported Identity Wallet address does not match the bind address.")
                             print("Please try again with the correct seed phrase.")
@@ -98,7 +101,7 @@ class WalletGenerator:
                         print(f"\033[1mMiner ID: {miner_id}\033[0m - Binding identity wallet found")
                         seed_phrase, iw_address = self.read_wallet_file(file_path)
 
-                        bind_iw_address = self.contract.functions.identityAddress(rw_address).call()
+                        bind_iw_address = self.fetch_iw_address(rw_address)
                         if iw_address.lower() != bind_iw_address.lower():  # Compare in lowercase
                             raise ValueError(f"Mismatch found for Miner ID {miner_id}.")
                         
@@ -112,27 +115,30 @@ class WalletGenerator:
                 self.print_table(data)
 
     def validate_miner_keys(self, miner_ids):
+        # miner_id may be composite address, e.g., miner_id = 0x1234-GPUNAME"
         for miner_id in miner_ids:
-            file_path = os.path.join(self.keys_dir, f'{miner_id.lower()}.txt')
+            reward_wallet = miner_id.split('-')[0].lower()  # Extract the address
+            file_path = os.path.join(self.keys_dir, f'{reward_wallet}.txt')
 
             if not os.path.exists(file_path):
-                print(f"ERROR: Identity wallet file missing for Miner ID {miner_id}. Exiting...")
-                raise ValueError(f"Identity wallet file not found in ~/.heurist-keys for Miner ID {miner_id}. Run `python3 auth/generator.py` to generate the identity wallet.")
+                print(f"ERROR: Identity wallet file missing for Reward Wallet {reward_wallet}. Exiting...")
+                raise ValueError(f"Identity wallet file not found in ~/.heurist-keys for Reward Wallet {reward_wallet}. Run `python3 auth/generator.py` to generate the identity wallet.")
 
             seed_phrase, iw_address = self.read_wallet_file(file_path)
 
-            if not self.is_bind(miner_id):
-                print(f"Warning: Identity wallet binding is not established for Miner ID {miner_id}. The binding will be effective after completion of some compute jobs.")
+            if not self.is_bind(reward_wallet):
+                print(f"Warning: Identity wallet binding is not established for Reward Wallet {reward_wallet}. The binding will be effective after completion of some compute jobs.")
             else:
-                bind_iw_address = self.contract.functions.identityAddress(miner_id).call()
+                bind_iw_address = self.fetch_iw_address(reward_wallet)
                 if iw_address.lower() != bind_iw_address.lower():
-                    print(f"ERROR: Identity wallet mismatch found for Miner ID {miner_id}. Exiting...")
-                    raise ValueError(f"Identity wallet mismatch found for Miner ID {miner_id}.")
+                    print(f"ERROR: Identity wallet mismatch found for Reward Wallet {reward_wallet}. Exiting...")
+                    raise ValueError(f"Identity wallet mismatch found for Reward Wallet {reward_wallet}.")
 
             print(f"MINER ID {miner_id} authenticated. Proceed to mining.")
 
     def generate_signature(self, miner_id):
-        file_path = os.path.join(self.keys_dir, f'{miner_id.split("-")[0].lower()}.txt')
+        reward_wallet = miner_id.split('-')[0].lower()  # Extract the address
+        file_path = os.path.join(self.keys_dir, f'{reward_wallet}.txt')
         seed_phrase, iw_address = self.read_wallet_file(file_path)
         
         self.w3.eth.account.enable_unaudited_hdwallet_features()
@@ -141,7 +147,7 @@ class WalletGenerator:
         current_time_secs = int(time.time())
         hourly_time = current_time_secs - (current_time_secs % 3600)  # Unix Timestamp Round down to the nearest hour
         
-        message = f"{miner_id.split('-')[0].lower()}-{hourly_time}" # Always lower case
+        message = f"{reward_wallet}-{hourly_time}" # Always lower case
         signable_message = encode_defunct(text=message)
         signature = self.w3.eth.account.sign_message(signable_message, private_key=private_key)
         

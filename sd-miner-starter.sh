c7db68a0e01e9054d615b2332d2e6cbae35cf917
tsubasakong <tahe.nft@gmail.com>
2024-09-09 18:32:38 +0000

    specify to use flux-dev-4bit

--- sd-miner-starter.sh
+++ sd-miner-starter.sh
@@ -15,12 +15,12 @@ done
 sed -i "s/num_cuda_devices = .*/num_cuda_devices = $MINER_COUNT/" /app/config.toml
 
 # Set default values for arguments
-LOG_LEVEL=${LOG_LEVEL:-"INFO"}
+LOG_LEVEL=${LOG_LEVEL:-"DEBUG"}
 AUTO_CONFIRM=${AUTO_CONFIRM:-"yes"}
 EXCLUDE_SDXL=${EXCLUDE_SDXL:-"no"}
 SKIP_SIGNATURE=${SKIP_SIGNATURE:-"yes"}
 SKIP_CHECKSUM=${SKIP_CHECKSUM:-"yes"}
-MODEL_ID=${MODEL_ID:-""}
+MODEL_ID=${MODEL_ID:-"FLUX.1-dev-4bit "}
 CUDA_DEVICE_ID=${CUDA_DEVICE_ID:-""}
 
 # Construct the command
@@ -29,10 +29,10 @@ CMD="python3 sd-miner-v1.3.1.py --log-level $LOG_LEVEL"
 # Add optional flags
 [ "$AUTO_CONFIRM" = "yes" ] && CMD="$CMD --auto-confirm yes"
 [ "$EXCLUDE_SDXL" = "yes" ] && CMD="$CMD --exclude-sdxl"
+[ ! -z "$CUDA_DEVICE_ID" ] && CMD="$CMD --cuda-device-id $CUDA_DEVICE_ID"
+[ ! -z "$MODEL_ID" ] && CMD="$CMD --model-id $MODEL_ID"
 [ "$SKIP_SIGNATURE" = "yes" ] && CMD="$CMD --skip-signature"
 [ "$SKIP_CHECKSUM" = "yes" ] && CMD="$CMD --skip-checksum"
-[ ! -z "$MODEL_ID" ] && CMD="$CMD --model-id $MODEL_ID"
-[ ! -z "$CUDA_DEVICE_ID" ] && CMD="$CMD --cuda-device-id $CUDA_DEVICE_ID"
 
 # Execute the command
 exec $CMD
